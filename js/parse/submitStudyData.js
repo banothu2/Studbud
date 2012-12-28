ea876bf242d072b09f53a2e2d1e6b1128f0f8fcd
Kirthi Banothu <banothu2@illinois.edu>
2012-12-27 19:22:56 -0600

    fuckk

--- js/parse/submitStudyData.js
+++
@@ -1,9 +0,0 @@
-Parse.initialize("78z9KHgSR3zIeFF7soCrOiHE6D99hNTJOmnJ2oay", "E8xzL8zlCs6GFfA2kEjAuBdedyZ9vS8ErBI3vT71");
-
-var currentUser = Parse.User.current();
-if(currentUser){
-
-} else {
-	// redirect page to login page 
-	window.location.replace("login.html");
-}
