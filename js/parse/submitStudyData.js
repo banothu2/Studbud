4320b2855376c4cf5d9dd815b1c273029f55b187
Kirthi Banothu <banothu2@illinois.edu>
2012-12-29 00:44:22 -0600

    Maps with ability to add data points
    Work in progress - Decided to use parse for databrowser for time being.

--- js/parse/submitStudyData.js
+++ js/parse/submitStudyData.js
@@ -1,9 +1,43 @@
 Parse.initialize("78z9KHgSR3zIeFF7soCrOiHE6D99hNTJOmnJ2oay", "E8xzL8zlCs6GFfA2kEjAuBdedyZ9vS8ErBI3vT71");
 
-var currentUser = Parse.User.current();
-if(currentUser){
+var currentUser = Parse.User.current(); //Checks to see if user is logged in
+	if(currentUser){
+		// when logged in:
 
-} else {
-	// redirect page to login page 
-	window.location.replace("login.html");
-}
+		$(document).ready(//Wait for document to be ready
+			function submitOrder() {
+
+					$("button").click(
+
+						function submitMapper(){
+
+			//				Parse.User.logIn($("#inputUsername").val(), $("#inputPassword").val(), {
+							var Mapper = Parse.Object.extend("mapper");
+							var mapper = new Mapper();
+							 
+							mapper.save({
+							  latitude: $("#inputLatitude").val(),
+							  longitude: $("#inputLongitude").val(),
+							  notes: $("#inputNotes").val(),
+							  subject: $("#inputSubject").val()
+							}, {
+							  success: function(map) {
+							    // The object was saved successfully.
+							    alert("worked!");
+							  },
+							  error: function(map, error) {
+							    // The save failed.
+							    alert("failed");
+							    // error is a Parse.Error with an error code and description.
+							  }
+							}); 
+
+						}	
+					)
+			}
+		);
+	}	
+	else {
+		// redirect to login page 
+		window.location.replace("login.html");
+	}
